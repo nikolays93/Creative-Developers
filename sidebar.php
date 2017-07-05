5f5a8859e5e92392b20bf3e332e00979c6ce7bc3
NikolayS93 <nikolays93@ya.ru>
2017-07-06 01:02:57 +0400

    fix conditions

--- sidebar.php
+++ sidebar.php
@@ -2,21 +2,27 @@
 if (!is_active_sidebar('archive') && !is_active_sidebar('woocommerce') )
 	return;
 
-add_action( 'before_sidebar', 'aside_start', 10 );
 function aside_start(){
 	echo '</div>';
 	echo '<div id="secondary" class="col-3">';
 	echo '<aside class="widget-area" role="complementary">';
 }
 
-add_action( 'after_sidebar',  'aside_end', 10 );
 function aside_end(){
 	echo '</aside>';
 }
 
+$is_commerce = function_exists('is_woocommerce') ?
+	(is_woocommerce() || is_shop() || is_cart() || is_checkout() || is_account_page()) : false;
+
+if( !$is_commerce ){
+	add_action( 'before_sidebar', 'aside_start', 10 );
+	add_action( 'after_sidebar',  'aside_end', 10 );
+}
+
 do_action('before_sidebar');
 
-if ( function_exists('is_woocommerce') && is_woocommerce() ){
+if (  $is_commerce ){
 	dynamic_sidebar( 'woocommerce' );
 }
 else {
