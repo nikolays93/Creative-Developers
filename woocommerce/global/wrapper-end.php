ead2b710cbf61e3cd6fafda069500f0f132044df
NikolayS93 <nikolays93@ya.ru>
2017-07-05 21:45:29 +0400

    bugfix

--- woocommerce/global/wrapper-end.php
+++ woocommerce/global/wrapper-end.php
@@ -6,7 +6,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 if( is_active_sidebar('woocommerce') && apply_filters( 'sidebar_on_single', !is_singular( 'product' ) ) )
 	do_action( 'woocommerce_sidebar' );
 ?>
-?>
 		</div><!-- .col -->
 	</div><!-- .row -->
 </div><!-- .container -->
