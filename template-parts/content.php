93bb017873d435a93f24b0638c4b81a991ec5708
Nikolays93 <Nikolays93@ya.ru>
2017-10-17 20:47:51 +0400

    _-_-_

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,11 +1,7 @@
-<?php
-	$post_class = ( is_singular() ) ? '' : 'media ';
-	$post_class .= get_default_bs_columns(apply_filters( 'content_columns', 2 ));
-?>
-			<article <?php post_class($post_class); ?>>
-				<?php the_thumbnail(); ?>
-				<div class="media-body article-content">
-					<?php the_advanced_title(); ?>
-					<?php the_content('<span class="more meta-nav">Подробнее</span>'); ?>
-				</div>
-			</article>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php the_thumbnail(); ?>
+    <div class="media-body article-content">
+        <?php the_advanced_title(); ?>
+        <?php the_content('<span class="more meta-nav">Подробнее</span>'); ?>
+    </div>
+</article>
