da67ae5a4126434203a05bddb3a18992292d1071
Nickolays93 <nikolays93@ya.ru>
2018-06-25 16:38:28 +0400

    add schema

--- header.php
+++ header.php
@@ -10,32 +10,39 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="site">
 		<div class="container site-header">
-			<div class="row head-info">
-				<div class="col-4 logotype">
-				<?php
-					the_custom_logo();
-				?>
-				</div>
-				<div class="col-4 contacts">
+			<div itemscope itemtype="http://schema.org/LocalBusiness">
+				<div class="row head-info">
+					<div class="col-4 logotype">
 					<?php
-					 /**
-					   * From Organized Contacts Plug-in
-					   */
-					  if( shortcode_exists( 'company' ) ){
-					    echo do_shortcode('[company field="address"]');
-					    echo do_shortcode('[company field="numbers"]');
-					    echo do_shortcode('[company field="email"]');
-					    echo do_shortcode('[company field="time_work"]');
-					    echo do_shortcode('[company field="socials"]');
-
-					    echo do_shortcode('[phone del="," num="1"]'); // only first phone between ,
-					  }
+						echo do_shortcode('[company field="image"]');
 					?>
+					</div>
+					<div class="col-4 contacts">
+						<?php
+						 /**
+						   * From Organized Contacts Plug-in
+						   */
+						  if( shortcode_exists( 'company' ) ) {
+						  	echo do_shortcode('[company field="name"]');
+						    echo do_shortcode('[company field="address"]');
+						    echo do_shortcode('[company field="numbers"]');
+						    echo do_shortcode('[company field="email"]');
+						    echo do_shortcode('[company field="time_work"]');
+						    echo do_shortcode('[company field="socials"]');
+
+						    // echo do_shortcode('[phone del="," num="1"]'); // only first phone between ,
+						  }
+						?>
+					</div>
+					<div class="col-4 callback">
+						<!-- <a href="#" id="get-recall"></a> -->
+					</div>
+				</div><!--.row head-info-->
+
+				<div class="hidden-xs-up">
+					<span itemprop="priceRange">RUB</span>
 				</div>
-				<div class="col-4 callback">
-					<!-- <a href="#" id="get-recall"></a> -->
-				</div>
-			</div><!--.row head-info-->
+			</div>
 		</div>
 		<?php default_theme_nav(); ?>
 		<div id="content" class="site-content">
